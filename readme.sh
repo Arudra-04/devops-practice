97d1d7585c4a54083903a988505cddc9cccb7ec9
Arudra-04 <84491907+Arudra-04@users.noreply.github.com>
2021-05-22 13:38:57 +0530

    Create readme.sh

---
+++ readme.sh
@@ -0,0 +1,7 @@
+echo "# devops-practice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Arudra-04/devops-practice.git
+git push -u origin main
